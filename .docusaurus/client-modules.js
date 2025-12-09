887ccec406623ba21deb6a49e3db31e8f7899c4b
alisa.wu <alisa.wu@airudder.com>
2025-12-09 15:35:48 +0800

    feat: init项目

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/wuyanhua/Documents/project/Docusaurus-Demo/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/wuyanhua/Documents/project/Docusaurus-Demo/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/wuyanhua/Documents/project/Docusaurus-Demo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/wuyanhua/Documents/project/Docusaurus-Demo/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/wuyanhua/Documents/project/Docusaurus-Demo/src/css/custom.css"),
+];
